c77360bf9ee4a4c76eab83b45020e50ba59971f8
Anand <anandveeraswamy@gmail.com>
2026-02-10 08:51:39 +0000

    Ensure users do not use the same email to register again

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -4,16 +4,21 @@ from django.contrib.auth.password_validation import validate_password
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
-    email = serializers.EmailField(required=False, allow_blank=True)
+    email = serializers.EmailField(required=True)
 
     class Meta:
         model = User
         fields = ('username', 'password', 'email')
 
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("A user with this email already exists.")
+        return value
+
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
-            email=validated_data.get('email', ''),
+            email=validated_data['email'],
             password=validated_data['password']
         )
         return user
